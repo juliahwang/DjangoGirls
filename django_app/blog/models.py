081e421318125b074a93489286bb3a124c0e50fa
juliahwang <qufskan9396@gmail.com>
2017-05-29 15:20:11 +0900

    Created model, admin register Post'

--- django_app/blog/models.py
+++ django_app/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.utils import timezone  # 장고모델 안에 현재시간을 알려주는 모듈을 불러옴.
 # Create your models here.
+
+
+class Post(models.Model):  # models모듈 안에 Model 클래스를 상속받는다.
+    """
+    기본 테이블 하나를 만드는 클래스
+    """
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now
+    )
+    published_date = models.DateTimeField(
+        blank=True, null=True
+    )
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
